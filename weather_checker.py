11005aa54093535114121fbbab1b099cd3b111d6
Jordan Landel <jordan.landel26@utexas.edu>
2021-05-20 19:39:45 -0500

    added get_pretty_weather func to simplify weather output

--- weather_checker.py
+++ weather_checker.py
@@ -2,24 +2,34 @@ import click
 import os
 from dotenv import load_dotenv
 import requests
+from pprint import pprint
 
 load_dotenv()
 
 OPEN_WEATHER_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
 
 def create_url_from_city_name(city_name):
-    return ''
+    city_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city_name,OPEN_WEATHER_API_KEY)
+    return city_url
 
 def get_weather_from_city_name(city_name):
     url = create_url_from_city_name(city_name)
     weather_request = requests.get(url)
     return weather_request.json()
 
+def get_pretty_weather(weather):
+    city = weather['name']
+    temperature = (weather['main']['temp']-273.15)*(9/5)+32
+    weather_conditions = weather['weather'][0]['description']
+    pretty_weather = {'City':city,'Temperature':temperature,'Weather_Conditions':weather_conditions}
+    return pretty_weather
+
 @click.command()
 @click.argument('city_name')
 def main(city_name):
     weather = get_weather_from_city_name(city_name)
-    click.echo("The weather in {} is {}".format(city_name, weather))
+    pweather = get_pretty_weather(weather)
+    pprint(pweather)
 
 if __name__ == "__main__":
     main()
